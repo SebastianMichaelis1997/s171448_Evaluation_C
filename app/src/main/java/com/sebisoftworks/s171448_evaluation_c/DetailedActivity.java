ecb3630d4ca596912c66e3aa4311799e74872a63
SebastianMichaelis1997 <sebastian@sileachim.de>
2020-01-25 12:33:50 +0100

    Finished Task and added better Handling of no new Values

--- app/src/main/java/com/sebisoftworks/s171448_evaluation_c/DetailedActivity.java
+++ app/src/main/java/com/sebisoftworks/s171448_evaluation_c/DetailedActivity.java
@@ -2,6 +2,9 @@ package com.sebisoftworks.s171448_evaluation_c;
 
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,24 +14,36 @@ import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class DetailedActivity extends AppCompatActivity implements Runnable {
+public class DetailedActivity extends AppCompatActivity implements Runnable, SensorEventListener {
     ArrayAdapter<String> mArrayAdapter;
     ArrayList<String> mData;
     Handler mHandler;
     ListView listView;
     String name;
+    ArrayList<Float> sensorData;
+    int sek = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Intent i = getIntent();
-        name = i.getStringExtra("sensorName");
+        Intent intent = getIntent();
+        name = intent.getStringExtra("sensorName");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed);
         listView = findViewById(R.id.list_detail);
         mData = new ArrayList<>();
-        mData.add("Sensor Data for "+name);
+        sensorData = new ArrayList<>();
+        sensorData.add(0.0F);
         SensorManager sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sm.getSensorList(Sensor.TYPE_ALL);
+        for (int i = 0; i < sensors.size(); i++) {
+            if (sensors.get(i).getName().equals(name)) {
+                sm.registerListener(this, sensors.get(i), SensorManager.SENSOR_DELAY_NORMAL);
+                break;
+            }
+        }
         mArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mData);
         listView.setAdapter(mArrayAdapter);
         mHandler = new Handler();
@@ -37,8 +52,29 @@ public class DetailedActivity extends AppCompatActivity implements Runnable {
 
     @Override
     public void run() {
-        mData.add("Sensor Data for "+name);
+        Float average = 0.0F;
+        ArrayList<Float> vals = sensorData;
+        sensorData = new ArrayList<>();
+        if (vals.size() > 0) {
+            for (int i = 0; i < vals.size(); i++) {
+                average += vals.get(i);
+            }
+            Float Onaverage = average / ((0.0F + vals.size()));
+            mData.add("Sec " + sek++ + ": " + Onaverage);
+        } else {
+            mData.add("Sec " + sek + ": No new Values in Second " + sek++);
+        }
         mArrayAdapter.notifyDataSetChanged();
         mHandler.postDelayed(this, 1000);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        sensorData.add(event.values[0]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
